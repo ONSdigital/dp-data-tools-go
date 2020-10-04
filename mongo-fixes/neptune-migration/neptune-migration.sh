a6d7249a06812c08a5f9e57ee921853fa5675653
CarlHembrough <carlhuk@gmail.com>
2020-10-04 15:38:01 +0100

    Add interpreter line to bash script
    + Use double square brackets in parameter check

--- mongo-fixes/neptune-migration/neptune-migration.sh
+++ mongo-fixes/neptune-migration/neptune-migration.sh
@@ -1,5 +1,6 @@
+#!/usr/bin/env bash
 
-if [ -z "$1" ]
+if [[ -z "$1" ]]
   then
     echo "Please supply the mongo connection string as the first parameter, e.g mongodb://localhost:27017"
     exit 1
